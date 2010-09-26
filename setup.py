8c535c5971250420cbbb15b49d98c1a7a02ab1f3
idle sign <idlesign@yandex.ru>
2010-09-26 10:23:19 +0700

    * chg: обновлен setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 from pyyaru import VERSION
 
-f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+f = open(os.path.join(os.path.dirname(__file__), 'README'))
 readme = f.read()
 f.close()
 
